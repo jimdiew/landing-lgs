429f29be23019e0418e0a9ee2c0085c837dc3986
Jimena Diewiatajew <jimediew@gmail.com>
2024-09-22 21:18:27 -0300

    footer

--- landing/src/components/Footer/footer.tsx
+++ landing/src/components/Footer/footer.tsx
@@ -1,6 +1,12 @@
-import { Grid2 } from "@mui/material"
+import { Grid2, Typography } from "@mui/material"
 const Footer = () => {
-  return <Grid2></Grid2>
+  return (
+    <Grid2 container justifyContent={"center"} pt={"2rem"} pb={"2rem"}>
+      <Typography variant="body2" color="white">
+        info@lgs-companion.com
+      </Typography>
+    </Grid2>
+  )
 }
 
 export default Footer
